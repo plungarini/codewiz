2a6f4ebae5a5ba81f48189044f2fa2a1bcc47a9d
Pietro Lungarini <pietro@lungarini.it>
2023-05-26 01:46:59 +0200

    style: Improve message component layout and code highlighting styles

--- src/app/core/pages/chat/components/messages/messages.component.ts
+++ src/app/core/pages/chat/components/messages/messages.component.ts
@@ -8,7 +8,7 @@ import { AiChatMessage, AiChatMessageRole } from 'src/app/shared/models/ai-chat/
   templateUrl: './messages.component.html',
 	styles: [`
 		:host {
-			@apply block overflow-hidden pb-16 pt-2 max-h-full overflow-y-auto relative;
+			@apply block overflow-x-hidden overflow-y-visible pb-44 max-h-full relative w-full;
 		}
 	`],
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -30,18 +30,30 @@ export class MessagesComponent {
 		this.markdownService.renderer.code = (code, lang, isEscaped) => {
 			const highlighted = hljs.highlightAuto(code, lang ? [lang] : []).value;
 			const klass = lang ? `${lang} ` : '';
-			return `
-				<div class="w-full my-3">
-					<div class="w-full px-2.5 py-2 bg-zinc-700 rounded-t-md">
+
+			if (!lang) {
+				return `
+					<div class="w-full my-3 relative group">
+						<pre><code class="hljs ${klass} !bg-zinc-950 rounded-lg break-words min-w-full whitespace-pre-wrap flex w-full max-w-full">${highlighted}</code></pre>
+					</div>
+				`;
+			}
+
+			return  `
+				<div class="w-full my-3 relative group">
+					<div class="w-full px-2.5 py-2 bg-zinc-700 rounded-t-lg">
 						<p class="text-zinc-300 -mt-[0.2rem] text-sm">${lang || 'Code Snippet'}</p>
 					</div>
-					<pre><code class="hljs ${klass} !bg-zinc-950 rounded-b-md break-words min-w-full whitespace-pre-wrap flex w-full max-w-full">${highlighted}</code></pre>
+					<pre class="whitespace-pre overflow-x-auto"><code class="hljs ${klass} !bg-zinc-950 rounded-b-lg break-words min-w-full flex w-full max-w-full">${highlighted}</code></pre>
 				</div>
 			`;
 		}
 		this.markdownService.renderer.codespan = (code) => {
 			const highlighted = hljs.highlightAuto(code).value;
-			return `<code class="hljs !bg-zinc-950 rounded-md break-words whitespace-pre-wrap inline-flex max-w-full">${highlighted}</code>`;
+			return `<code class="hljs !bg-zinc-950 rounded-md selection:bg-fuchsia-300 selection:text-fuchsia-900 leading-5 my-[0.1rem] break-words whitespace-pre-wrap inline-flex max-w-full">${highlighted}</code>`;
+		}
+		this.markdownService.renderer.paragraph = (text) => {
+			return `<p class="mt-4 first-of-type:mt-0 whitespace-pre-wrap">${text}</p>`;
 		}
 	}
 
