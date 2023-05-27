72a55cab002ad7ff3f5044a3cff3248d5b19b534
Pietro Lungarini <pietro@lungarini.it>
2023-05-27 23:54:40 +0200

    refactor: simplify codeBlockAndHeader and codeBlockPlain functions

--- src/app/core/pages/chat/components/messages/md-blocks/code-block.md.ts
+++ src/app/core/pages/chat/components/messages/md-blocks/code-block.md.ts
@@ -1,14 +1,14 @@
-export const codeBlockAndHeader = (lang: string, klass: string, code: string) => `
+export const codeBlockAndHeader = (lang: string, code: string) => `
 	<div class="w-full my-3 relative group">
 		<div class="w-full px-2.5 py-2 bg-zinc-700 rounded-t-lg">
 			<p class="text-zinc-300 -mt-[0.2rem] text-sm">${lang || 'Code Snippet'}</p>
 		</div>
-		<pre class="whitespace-pre overflow-x-auto"><code class="hljs ${klass} !bg-zinc-950 rounded-b-lg break-words min-w-full flex w-full selection:bg-sky-900/90 selection:text-sky-400 max-w-full">${code}</code></pre>
+		<pre class="whitespace-pre overflow-x-auto"><code class="hljs ${lang ? `${lang} ` : ''}!bg-zinc-950 rounded-b-lg break-words min-w-full flex w-full selection:bg-sky-900/90 selection:text-sky-400 max-w-full">${code}</code></pre>
 	</div>
 `.trim();
 
-export const codeBlockPlain = (klass: string, code: string) => `
+export const codeBlockPlain = (code: string) => `
 	<div class="w-full my-3 relative group">
-		<pre><code class="hljs ${klass} !bg-zinc-950 rounded-lg break-words min-w-full whitespace-pre-wrap flex w-full max-w-full">${code}</code></pre>
+		<pre><code class="hljs !bg-zinc-950 rounded-lg break-words min-w-full whitespace-pre-wrap flex w-full max-w-full">${code}</code></pre>
 	</div>
 `.trim();
