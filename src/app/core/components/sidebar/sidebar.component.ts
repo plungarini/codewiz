bb7a28edbfab71ea48adc73e72ae75a62642e701
Pietro Lungarini <pietro@lungarini.it>
2023-06-07 17:39:56 +0200

    feat(sidebar): add navigation links and logo to sidebar

--- src/app/core/components/sidebar/sidebar.component.ts
+++ src/app/core/components/sidebar/sidebar.component.ts
@@ -1,17 +1,41 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styles: [
-    `
-      :host {
-        display: block;
-      }
-    `
-  ],
+  styleUrls: ['./sidebar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnDestroy {
+	routerSub: Subscription | undefined;
+	currentPage: string = 'home';
+
+	constructor(private router: Router, private cdRef: ChangeDetectorRef) {
+		this.routerSub = this.router.events
+      .pipe(
+        filter(
+          (event) =>
+            event instanceof NavigationEnd
+        )
+      )
+      .subscribe((e) => {
+				if (e instanceof NavigationEnd) {
+					if (e.url.includes('chat')) {
+						this.currentPage = 'chat';
+					} else if (e.url.includes('app')) {
+						this.currentPage = 'app';
+					} else {
+						this.currentPage = 'app';
+					}
+					this.cdRef.detectChanges();
+        }
+      });
+	}
+
+	ngOnDestroy(): void {
+		this.routerSub?.unsubscribe();
+	}
 
 }
