16e127415dae0671dd5e882d16d72712b10a159d
Pietro Lungarini <pietro@lungarini.it>
2023-06-11 00:26:53 +0200

    feat: add start new chat button and display chat history, enable selection of documentation in search bar

--- src/app/core/pages/chat/components/chat-sidebar/chat-sidebar.component.ts
+++ src/app/core/pages/chat/components/chat-sidebar/chat-sidebar.component.ts
@@ -1,4 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Observable, Subject, switchMap } from 'rxjs';
+import { SelectedDocs } from 'src/app/shared/models/select-docs.model';
+import { AiChatService } from 'src/app/shared/services/ai-chat.service';
 
 @Component({
   selector: 'app-chat-sidebar',
@@ -15,4 +18,22 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 })
 export class ChatSidebarComponent {
 
+	$reposChats: Observable<unknown[]>;
+	selectedDoc: SelectedDocs | undefined;
+
+	private _$selectedRepo = new Subject<string>();
+
+	constructor(
+		private chatService: AiChatService,
+	) {
+		this.$reposChats = this._$selectedRepo.asObservable().pipe(
+			switchMap((repo) => this.chatService.getRepoChats(repo))
+		)
+	}
+
+	selectNewDoc(doc: SelectedDocs): void {
+		console.log('Loading new docs...', doc);
+		this.selectedDoc = doc;
+		this._$selectedRepo.next(doc.id);
+	}
 }
