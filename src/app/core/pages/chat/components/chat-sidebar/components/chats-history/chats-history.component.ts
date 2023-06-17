96af341a48af8a86a8267043547210cb96820e30
Pietro Lungarini <pietro@lungarini.it>
2023-06-17 16:07:17 +0200

    refactor: Update chat history component to group chats by date and improve visual layout.

--- src/app/core/pages/chat/components/chat-sidebar/components/chats-history/chats-history.component.ts
+++ src/app/core/pages/chat/components/chat-sidebar/components/chats-history/chats-history.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input } from '@angular/core';
+import { AiUserRepoChat } from 'src/app/shared/models/ai-chat/ai-chat.model';
 import { AiChatService } from 'src/app/shared/services/ai-chat.service';
 
 @Component({
@@ -7,7 +8,7 @@ import { AiChatService } from 'src/app/shared/services/ai-chat.service';
   styles: [
     `
       :host {
-        @apply block mt-4;
+        @apply block mt-4 overflow-x-hidden overflow-y-scroll pr-5 shrink;
       }
     `
   ],
@@ -15,7 +16,9 @@ import { AiChatService } from 'src/app/shared/services/ai-chat.service';
 })
 export class ChatsHistoryComponent {
 
-	@Input('chatHistory') set setChatHistory(value: any[]) {
+	groupedDocuments: { date: string, documents: AiUserRepoChat[] }[] = [];
+
+	@Input('chatHistory') set setChatHistory(value: AiUserRepoChat[]) {
 		if (!value) {
 			this.chatHistory = [];
 			return
@@ -26,9 +29,36 @@ export class ChatsHistoryComponent {
 			}
 			return c;
 		});
+
+		// Group documents by date
+		const grouped: Record<string, AiUserRepoChat[]> = {};
+		this.chatHistory.forEach((c) => {
+			const updatedAt = c.updatedAt?.toDate() || new Date();
+			const now = new Date();
+			const diffInDays = (now.getTime() - updatedAt.getTime()) / (1000 * 60 * 60 * 24);
+			let key;
+			if (diffInDays < 7) {
+				// If updated less than 1 week ago, group by date
+				key = updatedAt.toDateString();
+			} else {
+				// Otherwise, group by month and year
+				key = `${updatedAt.toLocaleString('default', { month: 'short' })} ${updatedAt.getFullYear()}`;
+			}
+			if (!grouped[key]) {
+				grouped[key] = [];
+			}
+			grouped[key].push(c);
+		});
+
+		this.groupedDocuments = Object.keys(grouped).map((key) => {
+			return {
+				date: key,
+				documents: grouped[key],
+			};
+		});
 	};
 
-	chatHistory: any[] = [];
+	chatHistory: AiUserRepoChat[] = [];
 	currentChatId: string = '';
 	
 	private queue: Set<string> = new Set();
@@ -38,7 +68,7 @@ export class ChatsHistoryComponent {
 		private cdRef: ChangeDetectorRef,
 	) {	}
 
-	trackBy(i: number, obj: any): string {
+	trackBy(i: number, obj: AiUserRepoChat): string {
 		return obj?.id || i.toString();
 	}
 
