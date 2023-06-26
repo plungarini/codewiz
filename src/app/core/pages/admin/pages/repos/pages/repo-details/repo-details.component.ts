9b1e30616efaded33b026f151f71e1e9a7f4a929
Pietro Lungarini <pietro@lungarini.it>
2023-06-27 00:28:46 +0200

    feat(repo-details): Add new and edit repo functionality with preview and meta sections

--- src/app/core/pages/admin/pages/repos/pages/repo-details/repo-details.component.ts
+++ src/app/core/pages/admin/pages/repos/pages/repo-details/repo-details.component.ts
@@ -1,4 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, of, switchMap } from 'rxjs';
+import { SelectedDocs } from 'src/app/shared/models/select-docs.model';
+import { FirebaseExtendedService } from 'src/app/shared/services/firebase-ext.service';
+import { Embedding } from './models/embedding.model';
+import { EmbeddingsService } from './services/embeddings.service';
 
 @Component({
   templateUrl: './repo-details.component.html',
@@ -12,5 +18,43 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RepoDetailsComponent {
+
+	repo$: Observable<SelectedDocs | undefined>;
+	embeddings: Embedding[] = [];
+	loadingError = '';
+	cacheId: string = '';
+	loading = false;
+
+	constructor(
+		private route: ActivatedRoute,
+		private db: FirebaseExtendedService,
+		private embeddingsService: EmbeddingsService,
+		private cdRef: ChangeDetectorRef,
+	) {
+		this.repo$ = this.route.paramMap.pipe(
+			switchMap((paramMap) => {
+				const id = (paramMap.get('id') || '').trim();
+				if (!id) return of(undefined);
+				/* this.loadEmbeddings(id); */
+				return this.db.getDoc<SelectedDocs>(`supported-docs/${id}`);
+			})
+		);
+	}
+
+	/* async loadEmbeddings(repo: string): Promise<void> {
+		if (this.cacheId == repo) return;
+		this.cacheId = repo;
+		this.loading = true;
+		this.cdRef.detectChanges();
+
+		try {
+			this.embeddings = await this.embeddingsService.getEmbeddings(repo);
+		} catch (err) {
+			this.loadingError = 'Error loading embeddings... Check console.';
+		} finally {
+			this.loading = false;
+			this.cdRef.detectChanges();
+		}
+	} */
 
 }
--- src/app/core/pages/admin/pages/repos/pages/repo-details/repo-details.module.ts
+++ src/app/core/pages/admin/pages/repos/pages/repo-details/repo-details.module.ts
@@ -1,17 +1,25 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
+import { ImgixAngularModule } from '@imgix/angular';
 import { RepoDetailsRoutingModule } from './repo-details-routing.module';
 import { RepoDetailsComponent } from './repo-details.component';
+import { RepoPreviewComponent } from './components/repo-preview/repo-preview.component';
+import { RepoMetaComponent } from './components/repo-meta/repo-meta.component';
 
 
 @NgModule({
   declarations: [
-    RepoDetailsComponent
+    RepoDetailsComponent,
+    RepoPreviewComponent,
+    RepoMetaComponent,
   ],
   imports: [
     CommonModule,
-    RepoDetailsRoutingModule
+		RepoDetailsRoutingModule,
+		ImgixAngularModule,
+		ReactiveFormsModule,
   ]
 })
 export class RepoDetailsModule { }
