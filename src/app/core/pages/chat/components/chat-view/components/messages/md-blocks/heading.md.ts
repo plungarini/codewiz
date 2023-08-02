a97ae0a65777769d452d431fa91d895ffab625bf
Pietro Lungarini <pietro@lungarini.it>
2023-08-02 17:45:07 +0200

    refactor: Simplify heading markdown formatting

--- src/app/core/pages/chat/components/chat-view/components/messages/md-blocks/heading.md.ts
+++ src/app/core/pages/chat/components/chat-view/components/messages/md-blocks/heading.md.ts
@@ -1,5 +1,5 @@
 import { paragraph } from './paragraph.md';
 
 export const heading = (text: string, level: number) => {
-	return level <= 3 ? `<h${level} class="mb-2 mt-4">${text}</h${level}>` : paragraph(`<strong class="text-zinc-200">${text}</strong>`);
+	return paragraph(`<strong class="text-zinc-200">${text}</strong>`);
 };
