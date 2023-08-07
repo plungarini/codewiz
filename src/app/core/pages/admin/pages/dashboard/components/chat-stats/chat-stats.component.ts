20e46ae592d85b90ae8defc65a038f8aec4be43f
Pietro Lungarini <pietro@lungarini.it>
2023-08-07 21:25:59 +0200

    feat: add admin chat stats component, model, custom pipe and service

---
+++ src/app/core/pages/admin/pages/dashboard/components/chat-stats/chat-stats.component.ts
@@ -0,0 +1,164 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RepoStat } from '../../models/chat-stats.model';
+import { ChatStatsService } from '../../services/chat-stats.service';
+
+@Component({
+  selector: 'app-chat-stats',
+  templateUrl: './chat-stats.component.html',
+  styles: [
+    `
+      :host {
+        display: block;
+      }
+    `
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ChatStatsComponent {
+
+	stats$ = this.chatStatsService.getAllChatStats();
+
+	constructor(
+		private chatStatsService: ChatStatsService,
+	) { }
+
+	getTotalCount(stats: RepoStat[]): number {
+		let totalCount: number = 0;
+		stats.forEach((repoStat: RepoStat) => {
+			const promptCount: number = repoStat.prompt?.count || 0;
+			const completionCount: number = repoStat.completion?.count || 0;
+			const itemTotalCount: number = promptCount + completionCount;
+			totalCount += itemTotalCount;
+		});
+		return totalCount;
+	}
+
+	getTotalCountLastMonth(stats: RepoStat[]): number {
+		let totalCount: number = 0;
+		stats.forEach((repoStat: RepoStat) => {
+			const historyItem = repoStat.history.find((item) => item.createdAt.toDate().getMonth() === new Date().getMonth() - 1);
+			if (!historyItem) return;
+			const promptCount: number = historyItem.prompt?.count || 0;
+			const completionCount: number = historyItem.completion?.count || 0;
+			const itemTotalCount: number = promptCount + completionCount;
+			totalCount += itemTotalCount;
+		});
+		return totalCount;
+	}
+
+	getTotalCost(stats: RepoStat[]): number {
+		let totalCost: number = 0;
+		stats.forEach((repoStat: RepoStat) => {
+			const promptTotalUSD: number = repoStat.prompt?.totalUSD || 0;
+			const completionTotalUSD: number = repoStat.completion?.totalUSD || 0;
+			const itemTotalCost: number = promptTotalUSD + completionTotalUSD;
+			totalCost += itemTotalCost;
+		});
+		return totalCost;
+	}
+
+	getTotalCostLastMonth(stats: RepoStat[]): number {
+		let totalCost: number = 0;
+		stats.forEach((repoStat: RepoStat) => {
+			const historyItem = repoStat.history.find((item) => item.createdAt.toDate().getMonth() === new Date().getMonth() - 1);
+			if (!historyItem) return;
+			const promptTotalUSD: number = historyItem.prompt?.totalUSD || 0;
+			const completionTotalUSD: number = historyItem.completion?.totalUSD || 0;
+			const itemTotalCost: number = promptTotalUSD + completionTotalUSD;
+			totalCost += itemTotalCost;
+		});
+		return totalCost;
+	}
+
+	getPricePerQuestion(stats: RepoStat[]): number {
+		let costs: number[] = [];
+		stats.forEach((repoStat: RepoStat) => {
+			const promptTotalUSD: number = repoStat.prompt?.totalUSD || 0;
+			const completionTotalUSD: number = repoStat.completion?.totalUSD || 0;
+			const itemTotalCost: number = promptTotalUSD + completionTotalUSD;
+			costs.push(itemTotalCost);
+		});
+
+		const sum = costs.reduce((a, b) => a + b, 0);
+
+		return sum / costs.length;
+	}
+
+	getPricePerQuestionLastMonth(stats: RepoStat[]): number {
+		let costs: number[] = [];
+		stats.forEach((repoStat: RepoStat) => {
+			const historyItem = repoStat.history.find((item) => item.createdAt.toDate().getMonth() === new Date().getMonth() - 1);
+			if (!historyItem) return;
+			const promptTotalUSD: number = historyItem.prompt?.totalUSD || 0;
+			const completionTotalUSD: number = historyItem.completion?.totalUSD || 0;
+			const itemTotalCost: number = promptTotalUSD + completionTotalUSD;
+			costs.push(itemTotalCost);
+		});
+
+		const sum = costs.reduce((a, b) => a + b, 0);
+		return costs.length > 0 ? sum / costs.length : 0;
+	}
+
+	getPricePerPrompt(stats: RepoStat[]): number {
+		let costs: number[] = [];
+		stats.forEach((repoStat: RepoStat) => {
+			costs.push(repoStat.prompt?.totalUSD || 0);
+		});
+
+		const sum = costs.reduce((a, b) => a + b, 0);
+
+		return sum / costs.length;
+	}
+
+	getPricePerPromptLastMonth(stats: RepoStat[]): number {
+		let costs: number[] = [];
+		stats.forEach((repoStat: RepoStat) => {
+			const historyItem = repoStat.history.find((item) => item.createdAt.toDate().getMonth() === new Date().getMonth() - 1);
+			if (!historyItem) return;
+			costs.push(repoStat.prompt?.totalUSD || 0);
+		});
+
+		const sum = costs.reduce((a, b) => a + b, 0);
+		return costs.length > 0 ? sum / costs.length : 0;
+	}
+
+	getPricePerCompletion(stats: RepoStat[]): number {
+		let costs: number[] = [];
+		stats.forEach((repoStat: RepoStat) => {
+			costs.push(repoStat.completion?.totalUSD || 0);
+		});
+
+		const sum = costs.reduce((a, b) => a + b, 0);
+
+		return sum / costs.length;
+	}
+
+	getPricePerCompletionLastMonth(stats: RepoStat[]): number {
+		let costs: number[] = [];
+		stats.forEach((repoStat: RepoStat) => {
+			const historyItem = repoStat.history.find((item) => item.createdAt.toDate().getMonth() === new Date().getMonth() - 1);
+			if (!historyItem) return;
+			costs.push(repoStat.completion?.totalUSD || 0);
+		});
+
+		const sum = costs.reduce((a, b) => a + b, 0);
+		return costs.length > 0 ? sum / costs.length : 0;
+	}
+
+	getPriceDifference(current: number, past: number): number {
+		if (past === 0) {
+			if (current === 0) {
+				return 0;
+			} else {
+				return 100;
+			}
+		} else {
+			return ((current - past) / Math.abs(past)) * 100;
+		}
+	}
+
+	trackBy(i: number, obj: RepoStat): string {
+		return obj.id || i.toString();
+	}
+
+}
---
+++ src/app/core/pages/admin/pages/dashboard/services/chat-stats.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { orderBy } from '@angular/fire/firestore';
+import { combineLatest, filter, map, Observable, of, switchMap } from 'rxjs';
+import { FirebaseExtendedService } from 'src/app/shared/services/firebase-ext.service';
+import { CompletionStat, RepoStat } from '../models/chat-stats.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatStatsService {
+
+	constructor(
+		private db: FirebaseExtendedService,
+	) { }
+
+	getChatStatByRepo(repo?: string): Observable<RepoStat | undefined> {
+		if (!repo) return of(undefined)
+		return this.db.getDoc<CompletionStat>(`stats/completions/repos/${repo}`).pipe(
+			switchMap((stat) => {
+				return this.db.getCol<CompletionStat>(`stats/completions/repos/${repo}/byDate`, 'id', orderBy('createdAt')).pipe(
+					map((date) => ({ ...stat, history: date }))
+				);
+			}),
+		)
+	}
+
+	getAllChatStats(): Observable<RepoStat[]> {
+		return this.db.getCol<RepoStat>('/stats/completions/repos').pipe(
+			switchMap(users => {
+				const observables = users
+					.filter(user => !!user.id)
+					.map(user =>
+						this.getChatStatByRepo(user.id)
+					);
+				return combineLatest(observables);
+			}),
+			filter(stat => !!stat),
+			map((stats) => stats as RepoStat[])
+		)
+	}
+}
