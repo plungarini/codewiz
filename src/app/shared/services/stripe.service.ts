4948146a7748d4783251e9e29e1e3d72ea3f8cea
Pietro Lungarini <pietro@lungarini.it>
2023-08-10 00:55:07 +0200

    feat: Add Stripe service for retrieving product information

---
+++ src/app/shared/services/stripe.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { StripeProduct } from '../models/stripe.model';
+import { FirebaseExtendedService } from './firebase-ext.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserSubscriptionService {
+
+	constructor(
+		private db: FirebaseExtendedService,
+	) { }
+
+	getProduct(id: string) {
+		return this.db.getDoc<StripeProduct>(`products/${id}`);
+	}
+}
