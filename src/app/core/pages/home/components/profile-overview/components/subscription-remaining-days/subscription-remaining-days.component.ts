e21520f4419877f7a17b4454163179c9e0b78b06
Pietro Lungarini <pietro@lungarini.it>
2023-08-10 00:56:19 +0200

    feat(subscription-remaining-queries): Add new component for displaying remaining queries

--- src/app/core/pages/home/components/profile-overview/components/subscription-remaining-days/subscription-remaining-days.component.ts
+++ src/app/core/pages/home/components/profile-overview/components/subscription-remaining-days/subscription-remaining-days.component.ts
@@ -1,24 +1,25 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnChanges } from '@angular/core';
 
 @Component({
-  selector: 'app-subscription-remaining-days',
-  templateUrl: './subscription-remaining-days.component.html',
+  selector: 'app-subscription-remaining-queries',
+  templateUrl: './subscription-remaining-queries.component.html',
   styles: [
     `
 			circle {
-				transition: stroke-dashoffset 0.5s;
+				transition: stroke-dashoffset 0.8s ease-in-out;
 			}
 
       :host {
-        @apply block w-full h-full;
+        @apply block max-w-full max-h-full;
 			}
     `
 	],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SubscriptionRemainingDaysComponent implements OnChanges {
+export class SubscriptionRemainingQueriesComponent implements OnChanges {
 	
-	@Input() percentage: number = 0;
+	@Input() percentage = 0;
+	@Input() remainingQueries = 0;
 
   // Assuming the circle radius is 50 and cx, cy (the center of the circle) is 50, 50
   readonly CIRCUMFERENCE: number = 2 * Math.PI * 50;
@@ -26,7 +27,7 @@ export class SubscriptionRemainingDaysComponent implements OnChanges {
 
   ngOnChanges() {
 		this.offset = this.CIRCUMFERENCE - (this.percentage / 100) * this.CIRCUMFERENCE;
-		this.cdRef.detectChanges();
+		this.cdRef.markForCheck();
   }
 	
 	constructor(
