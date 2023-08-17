ceb31051d0c1696838e37d9a3adf67b6a7d507d4
Pietro Lungarini <pietro@lungarini.it>
2023-08-17 18:47:43 +0200

    feat(admin): add maintenance page to admin routing and sidebar components

--- src/app/core/pages/admin/admin-routing.module.ts
+++ src/app/core/pages/admin/admin-routing.module.ts
@@ -18,7 +18,11 @@ const routes: Routes = [
 			{
 				path: 'users',
 				loadChildren: () => import('./pages/users/users.module').then(m => m.UsersModule),
-			}
+			},
+			{
+				path: 'maintenance',
+				loadChildren: () => import('./pages/maintenance/maintenance.module').then(m => m.MaintenanceModule),
+			},
 		]
 	}
 ];
