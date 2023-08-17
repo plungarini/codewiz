0b3a8ef7351652bc60bd4651b7d94a8e54d89fd2
Pietro Lungarini <pietro@lungarini.it>
2023-08-17 18:47:20 +0200

    refactor: Remove unused code in maintenance component and app-status model

--- src/app/shared/models/app-status.model.ts
+++ src/app/shared/models/app-status.model.ts
@@ -1,10 +1,7 @@
-import { Timestamp } from 'src/app/auth/models/timestamp.model';
 
 export type AppStatus = {
 	maintenance: {
 		active: boolean;
 		reason?: string;
-		start?: Timestamp;
-		end?: Timestamp;
 	};
 }
