9fdc96e646f2920e546f4d8fdd49f754c56d9c72
Pietro Lungarini <pietro@lungarini.it>
2023-08-17 16:45:53 +0200

    feat(user-header): add user header component with user details and external links

---
+++ src/app/core/pages/admin/pages/users/pages/user-details/components/user-header/user-header.component.ts
@@ -0,0 +1,23 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { User } from 'src/app/auth/models/user.model';
+
+@Component({
+  selector: 'app-user-header',
+  templateUrl: './user-header.component.html',
+  styles: [
+    `
+      :host {
+        display: block;
+      }
+    `
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class UserHeaderComponent {
+
+	@Input() user: User | undefined;
+
+	firebaseBaseUrl = 'https://console.firebase.google.com/u/0/project/code-whiz-ai/firestore/data/~2Fusers~2F';
+	stripeBaseUrl = 'https://dashboard.stripe.com/customers/';
+
+}
