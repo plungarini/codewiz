c770a42893a8ddc34cfef18bb9b8738ac5294d43
Pietro Lungarini <pietro@lungarini.it>
2023-08-17 16:46:03 +0200

    feat(user-overview): add user overview component with user details

---
+++ src/app/core/pages/admin/pages/users/pages/user-details/components/user-overview/user-overview.component.ts
@@ -0,0 +1,38 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { User } from 'src/app/auth/models/user.model';
+
+@Component({
+  selector: 'app-user-overview',
+  templateUrl: './user-overview.component.html',
+  styles: [
+    `
+      :host {
+				@apply block max-w-full overflow-x-auto;
+      }
+    `
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class UserOverviewComponent {
+
+	@Input() user: User | undefined;
+
+	getSubscriptionStatus(user: User) {
+		const sub = user.subscriptions?.find(s => ['active', 'trialing'].includes(s.status));
+		if (!sub) return 'Inactive';
+
+		const now = new Date().getTime();
+		const start = sub.current_period_start.toDate().getTime();
+		const end = sub.current_period_end.toDate().getTime();
+		const isActive = now >= start && now <= end;
+
+		if (!isActive) return 'Inactive';
+
+		return sub.status[0].toUpperCase() + sub.status.slice(1);
+	}
+
+	getRevenue(inEur: number, outUsd: number): number {
+		return (inEur * 1.10) - outUsd;
+	}
+
+}
