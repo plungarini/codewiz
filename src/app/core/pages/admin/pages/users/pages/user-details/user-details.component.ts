a80cbfb7bf04c02c579ecd92bbb2dec579d81b1b
Pietro Lungarini <pietro@lungarini.it>
2023-08-17 16:46:14 +0200

    feat(user-details): add user details layout and components

--- src/app/core/pages/admin/pages/users/pages/user-details/user-details.component.ts
+++ src/app/core/pages/admin/pages/users/pages/user-details/user-details.component.ts
@@ -1,4 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map, Subscription, switchMap } from 'rxjs';
+import { User } from 'src/app/auth/models/user.model';
+import { UsersService } from 'src/app/auth/services/users.service';
 
 @Component({
   selector: 'app-user-details',
@@ -12,6 +16,77 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class UserDetailsComponent {
+export class UserDetailsComponent implements OnDestroy {
+
+	user: User | undefined;
+
+	private id: string = '';
+	private userSub: Subscription;
+
+	constructor(
+		private route: ActivatedRoute,
+		private usersService: UsersService,
+		private cdRef: ChangeDetectorRef,
+	) {
+		this.userSub = this.route.params.pipe(
+			switchMap(params => {
+				this.id = params['id'];
+				return this.usersService.getWithInvoice(this.id);
+			}),
+			map(user => this.calculateUserRevenue(user))
+		).subscribe((user) => {
+			this.user = user;
+			this.cdRef.markForCheck();
+		});
+	}
+
+	ngOnDestroy(): void {
+		this.userSub.unsubscribe();
+	}
+
+	private calculateUserRevenue(user?: User): User | undefined {
+		if (!user) return user;
+		
+		let totalCost = 0;
+		let currentTotalCost = 0;
+		let totalPaid = 0;
+		let currentTotalPaid = 0;
+
+		const now = new Date();
+		const month = now.getMonth();
+		const dateId = `${month < 10 ? '0' : ''}${month}_${now.getFullYear()}`;
+
+		user.usages?.forEach(usage => {
+			usage.stats?.forEach(stat => {
+				const totalQuestion = (stat.completion?.usedUSD || 0) + (stat.prompt?.usedUSD || 0);
+				
+				if (dateId === stat.id) currentTotalCost += totalQuestion;
+				totalCost += totalQuestion;
+			})
+		});
+
+		user.subscriptions?.forEach(subscription => {
+			subscription.invoices?.forEach(invoice => {
+				const paidAt = invoice?.status_transitions?.paid_at;
+				if (!invoice || !invoice?.paid || !paidAt) return;
+
+				const invoiceNow = new Date(paidAt * 1000);
+				const invoiceMonth = invoiceNow.getMonth();
+				const invoiceDateId = `${invoiceMonth < 10 ? '0' : ''}${invoiceMonth}_${invoiceNow.getFullYear()}`;
+
+				if (dateId === invoiceDateId) currentTotalPaid += invoice.amount_paid
+				totalPaid += invoice.amount_paid;
+			})
+		});
+		
+		const revenue: User['revenueDetails'] = {
+			totalCost,
+			totalCostThisMonth: currentTotalCost,
+			totalPaid,
+			paidThisMonth: currentTotalPaid,
+		};
+
+		return { ...user, revenueDetails: revenue };
+	}
 
 }
--- src/app/core/pages/admin/pages/users/pages/user-details/user-details.module.ts
+++ src/app/core/pages/admin/pages/users/pages/user-details/user-details.module.ts
@@ -1,17 +1,25 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { ImgixAngularModule } from '@imgix/angular';
+import { UserActionsComponent } from './components/user-actions/user-actions.component';
+import { UserHeaderComponent } from './components/user-header/user-header.component';
+import { UserOverviewComponent } from './components/user-overview/user-overview.component';
 import { UserDetailsRoutingModule } from './user-details-routing.module';
 import { UserDetailsComponent } from './user-details.component';
 
 
 @NgModule({
   declarations: [
-    UserDetailsComponent
+		UserDetailsComponent,
+		UserHeaderComponent,
+		UserOverviewComponent,
+		UserActionsComponent,
   ],
   imports: [
     CommonModule,
-    UserDetailsRoutingModule
+		UserDetailsRoutingModule,
+		ImgixAngularModule,
   ]
 })
 export class UserDetailsModule { }
