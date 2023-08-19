dad1733d45a2d7ecb0ef2e2c29ece167af883c93
Pietro Lungarini <pietro@lungarini.it>
2023-08-19 09:21:06 +0200

    feat: add global menu on site

---
+++ src/app/site/components/menu/menu.component.ts
@@ -0,0 +1,67 @@
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, HostListener, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+@Component({
+	selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styles: [
+    `
+      :host {
+        @apply block fixed inset-x-0 top-0 z-[5000];
+      }
+    `
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MenuComponent implements OnDestroy {
+
+	isScrolled = false;
+	currentPage = 'home';
+	menuOpen = false;
+
+	private routerSub: Subscription;
+
+	constructor(
+		private router: Router,
+		private cdRef: ChangeDetectorRef,
+	) {
+		this.routerSub = this.router.events.subscribe(() => {
+			const url = this.router.url;
+			const id = url.split('/')[1] || 'home';
+			if (id && id !== this.currentPage) {
+				this.currentPage = id;
+				this.cdRef.markForCheck();
+			}
+		})
+	}
+
+	ngOnDestroy(): void {
+		this.routerSub.unsubscribe();
+	}
+
+  @HostListener('window:scroll')
+  onWindowScroll() {
+		this.manageScroll();
+	}
+
+	@HostListener('body:scroll')
+	onDocumentScroll() {
+		this.manageScroll();
+	}
+
+	toggleMenu(state?: boolean): void {
+    this.menuOpen = state !== undefined ? state : !this.menuOpen;
+    this.cdRef.detectChanges();
+	}
+	
+	private manageScroll(): void {
+		const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+		const condition = scrollPosition > 0;
+		if (this.isScrolled !== condition) {
+			this.isScrolled = condition;
+			this.cdRef.markForCheck();
+		}
+	}
+
+}
