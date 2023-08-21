f71d301f38cc18fc943ade97bff015de05a75d8f
Pietro Lungarini <pietro@lungarini.it>
2023-08-22 00:45:32 +0200

    refactor: Remove unused banner component and related files

--- src/app/core/pages/home/home.module.ts
+++ src/app/core/pages/home/home.module.ts
@@ -3,23 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { ImgixAngularModule } from '@imgix/angular';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { BannerComponent } from './components/banner/banner.component';
 import { IdeasComponent } from './components/ideas/ideas.component';
 import { LatestChatsComponent } from './components/latest-chats/latest-chats.component';
 import { ProfileOverviewComponent } from './components/profile-overview/profile-overview.component';
+import { SubscriptionOverviewComponent } from './components/subscription-overview/subscription-overview.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { SubscriptionOverviewComponent } from './components/subscription-overview/subscription-overview.component';
 
 
 @NgModule({
 	declarations: [
 		HomeComponent,
 		ProfileOverviewComponent,
-		BannerComponent,
 		LatestChatsComponent,
 		IdeasComponent,
-  SubscriptionOverviewComponent,
+  	SubscriptionOverviewComponent,
 	],
   imports: [
     CommonModule,
