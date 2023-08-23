1d5a652543e5fbcc79ecb592717700ca8d275c16
Pietro Lungarini <pietro@lungarini.it>
2023-08-23 22:12:39 +0200

    feat(stripe.service): add method to get all products with prices

--- src/app/shared/models/stripe.model.ts
+++ src/app/shared/models/stripe.model.ts
@@ -10,4 +10,31 @@ export type StripeProduct = {
 	};
 	role?: string;
 	tax_code: string;
-} 
+	price?: StripeProductPrice;
+}
+
+export type StripeProductPrice = {
+  type: string;
+  active: boolean;
+  tiers: any;
+  recurring: {
+		interval: string;
+		usage_type: string;
+		aggregate_usage: any;
+		trial_period_days: any;
+		interval_count: number;
+	};
+  billing_scheme: string;
+  transform_quantity: any;
+  metadata: any;
+  interval: string;
+  unit_amount: number;
+  tax_behavior: string;
+  product: string;
+  trial_period_days: any;
+  currency: string;
+  interval_count: number;
+  tiers_mode: any;
+  description: any;
+	id: string;
+}
--- src/app/shared/services/stripe.service.ts
+++ src/app/shared/services/stripe.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
+import { combineLatest, map, Observable, switchMap } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { StripeProduct } from '../models/stripe.model';
+import { StripeProduct, StripeProductPrice } from '../models/stripe.model';
 import { FirebaseExtendedService } from './firebase-ext.service';
 
 @Injectable({
@@ -17,5 +18,18 @@ export class StripeService {
 	getProduct(id?: string) {
 		id = id || this.freePlanId;
 		return this.db.getDoc<StripeProduct>(`products/${id}`);
+	}
+
+	getAllProducts(): Observable<StripeProduct[]> {
+		return this.db.getCol<StripeProduct>('products').pipe(
+			switchMap(products => {
+				const observables = products.map(prod => {
+					return this.db.getCol<StripeProductPrice>(`products/${prod.id}/prices`).pipe(
+						map((prices) => ({ ...prod, price: prices.filter(p => p.active)[0] }))
+					)
+				});
+				return combineLatest(observables);
+			})
+		);
 	}
 }
