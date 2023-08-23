bee76bc9128881a773ff2c4f5b5d18d470854494
Pietro Lungarini <pietro@lungarini.it>
2023-08-23 22:18:01 +0200

    chore: Remove unnecessary code and update blocking map in index.html

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -21,50 +21,12 @@ export class AuthComponent implements OnInit, OnDestroy {
 	
 	alertSub: Subscription | undefined;
 
-	private observer: MutationObserver | undefined;
-
 	constructor(
 		private cdRef: ChangeDetectorRef,
 		private passwordResetAlert: PasswordResetAlertService,
 		@Inject(DOCUMENT) private document: Document,
 	) {
-		this.observer = new MutationObserver((mutationsList, observer) => {
-			for (const mutation of mutationsList) {
-				if (mutation.type === 'childList') {
-					const addedNodes = mutation.addedNodes;
-					addedNodes.forEach((node) => {
-						if (
-							node instanceof HTMLIFrameElement &&
-							(
-								node.src?.includes('https://codewiz.app/__/auth') ||
-								node.getAttribute('data-src')?.includes('https://codewiz.app/__/auth')
-							)
-						) {
-							const newSrc = node.src || node.getAttribute('data-src');
-							const alreadyAdded = document.body.querySelector('iframe[data-autoblock-bypass="true"]');
-							const isDifferent = newSrc !== alreadyAdded?.getAttribute('src');
-
-							if (!isDifferent || !newSrc) return;
-							
-							const newIframe = document.createElement('iframe');
-							Array.from(node.attributes).forEach((attr) => {
-								newIframe.setAttribute(attr.name, attr.value);
-							});
-							newIframe.removeAttribute('data-autoblocked');
-							newIframe.removeAttribute('data-src');
-							newIframe.setAttribute('src', newSrc);
-							newIframe.setAttribute('data-categories', 'essential');
-							newIframe.setAttribute('data-autoblock-bypass', 'true');
-
-							node.remove();
-							this.document.body.appendChild(newIframe);
-						}
-					})
-				}
-			}
-		});
-	
-		this.observer.observe(this.document.body, { childList: true, subtree: true });
+		
 	}
 	
 	ngOnInit(): void {		
@@ -76,7 +38,6 @@ export class AuthComponent implements OnInit, OnDestroy {
 
 	ngOnDestroy(): void {
 		this.alertSub?.unsubscribe();
-		this.observer?.disconnect();
 	}
 
 	checkPRCompletion(state: boolean): void {
