cbbe0953e45184a36882c23e392d48e7e04026fd
Pietro Lungarini <pietro@lungarini.it>
2023-08-25 21:27:57 +0200

    refactor: Rearrange component imports in pricing module

--- src/app/site/pages/pricing/pricing.module.ts
+++ src/app/site/pages/pricing/pricing.module.ts
@@ -1,19 +1,19 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { HeroComponent } from './components/hero/hero.component';
+import { PlanCardComponent } from './components/plans/plan-card/plan-card.component';
+import { PlansComponent } from './components/plans/plans.component';
 import { PricingRoutingModule } from './pricing-routing.module';
 import { PricingComponent } from './pricing.component';
-import { HeroComponent } from './hero/hero.component';
-import { PlansComponent } from './plans/plans.component';
-import { PlanCardComponent } from './plans/plan-card/plan-card.component';
 
 
 @NgModule({
   declarations: [
     PricingComponent,
-    HeroComponent,
-    PlansComponent,
-    PlanCardComponent
+		HeroComponent,
+		PlansComponent,
+		PlanCardComponent,
   ],
   imports: [
     CommonModule,
