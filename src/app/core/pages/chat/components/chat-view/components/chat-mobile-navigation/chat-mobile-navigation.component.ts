3da002783eef856b97e4b48b4d0e9f2addcca82e
Pietro Lungarini <pietro@lungarini.it>
2023-08-26 14:44:02 +0200

    feat: Add chat-mobile-navigation component HTML and TypeScript files

---
+++ src/app/core/pages/chat/components/chat-view/components/chat-mobile-navigation/chat-mobile-navigation.component.ts
@@ -0,0 +1,76 @@
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, Subject, switchMap } from 'rxjs';
+import { Repo } from 'src/app/shared/models/repo.model';
+import { AiChatService } from 'src/app/shared/services/ai-chat.service';
+
+@Component({
+  selector: 'app-chat-mobile-navigation',
+  templateUrl: './chat-mobile-navigation.component.html',
+  styles: [
+    `
+      :host {
+        @apply block left-0 w-full max-w-full fixed bottom-0 z-[80] pointer-events-none flex flex-col;
+      }
+    `
+	],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ChatMobileNavigationComponent implements OnInit {
+
+	@Output() onHideMobileMenu = new EventEmitter<void>();
+
+	show = false;
+	$reposChats: Observable<unknown[]>;
+	selectedDoc: Repo | undefined;
+
+	private _$selectedRepo = new Subject<string>();
+
+	constructor(
+		private chatService: AiChatService,
+		private router: Router,
+		private cdRef: ChangeDetectorRef,
+	) {
+		this.$reposChats = this._$selectedRepo.asObservable().pipe(
+			switchMap((repo) => this.chatService.getRepoChats(repo))
+		)
+	}
+
+	ngOnInit(): void {
+		setTimeout(() => {
+			this.show = true;
+			this.cdRef.markForCheck();
+		}, 100);
+	}
+
+	thisDate = () => Date.now();
+
+	startNewChat(): void {
+		this.router.navigateByUrl(`/app/chat/${this.selectedDoc?.id || 'angular'}/new`, { onSameUrlNavigation: 'reload' });
+		setTimeout(() => {
+			this.closeMobileMenu();
+		}, 300);
+		this.cdRef.markForCheck();
+	}
+
+	updateSelectedDoc(doc: Repo): void {
+		this.selectedDoc = doc;
+		this._$selectedRepo.next(doc.id);
+		
+		if (!this.router.url.includes(`/chat/${doc.id}/`)) {
+			this.router.navigateByUrl(`/app/chat/${doc.id}/new`);
+			this.cdRef.markForCheck();
+		}
+		this.cdRef.markForCheck();
+	}
+
+	async deleteChat(opt: { repo: string; id: string }): Promise<void> {
+		if (!opt) return console.error('Error on deleting chat, chat details are not defined', opt);
+		await this.chatService.deleteChat(opt.repo, opt.id);
+	}
+
+	closeMobileMenu(): void {
+		this.onHideMobileMenu.emit();
+	}
+
+}
