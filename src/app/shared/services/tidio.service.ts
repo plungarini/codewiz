71238ac3dfea7fe20878fa1a6764aad1741227bc
Pietro Lungarini <pietro@lungarini.it>
2023-09-07 16:39:25 +0200

    refactor(tidio.service.ts): optimize code for queue handling

--- src/app/shared/services/tidio.service.ts
+++ src/app/shared/services/tidio.service.ts
@@ -14,11 +14,45 @@ type TidioUser = {
 export class TidioService {
 
 	private production = environment.production;
+	private isReady = false;
+	private queuePurged = false;
+	private fnQueue: { fn: string, data: Partial<TidioUser> | undefined }[] = [];
+
+	private checkQueueInterval = setInterval(() => {
+		if (!this.isReady) return;
+		if (this.queuePurged || !this.production) {
+			clearInterval(this.checkQueueInterval);
+			return;
+		}
+		if (this.fnQueue.length <= 0) {
+			this.queuePurged = true;
+			return;
+		}
+
+		for (let i = 0; i < this.fnQueue.length; i++) {
+			const fn = this.fnQueue[i];
+			if (fn.fn === 'hide') {
+				this.hide();
+			} else if (fn.fn === 'show') {
+				this.show();
+			} else if (fn.fn === 'identify') {
+				this.identify(fn.data);
+			}
+		}
+
+		this.fnQueue = [];
+		this.queuePurged = true;
+	}, 500);
 
 	constructor() { }
 
 	hide() {
 		if (!this.production || !this._getConsent()) return;
+		this._initialize();
+		if (!this.isReady && !this.queuePurged) {
+			this._addToQueue('hide');
+			return;
+		}
 		const tidio = (window as any).tidioChatApi;
 		if (!tidio || !tidio?.hasOwnProperty('hide'))
 			return console.warn('[TIDIO] Unable to hide widget');
@@ -27,6 +61,11 @@ export class TidioService {
 	
 	show() {
 		if (!this.production || !this._getConsent()) return;
+		this._initialize();
+		if (!this.isReady && !this.queuePurged) {
+			this._addToQueue('show');
+			return;
+		}
 		const tidio = (window as any).tidioChatApi;
 		if (!tidio || !tidio?.hasOwnProperty('show'))
 			return console.warn('[TIDIO] Unable to show widget');
@@ -35,6 +74,11 @@ export class TidioService {
 
 	identify(user?: Partial<TidioUser>): void {
 		if (!this.production || !this._getConsent()) return;
+		this._initialize();
+		if (!this.isReady && !this.queuePurged) {
+			this._addToQueue('identify', user);
+			return;
+		}
 		const tidio = (window as any).tidioChatApi;
 		if (!tidio || !tidio?.hasOwnProperty('setVisitorData')) {
 			return console.warn('[TIDIO] Unable to set visitor data');
@@ -45,5 +89,16 @@ export class TidioService {
 	private _getConsent(): boolean {
 		const termly = (window as any)?.Termly?.getConsentState();
 		return !!termly?.performance;
+	}
+
+	private _initialize(): void {
+		if (this.isReady) return;
+		this.isReady = !!(window as any)?.tidioChatApi?.readyEventWasFired;
+	};
+
+	private _addToQueue(fn: string, data?: Partial<TidioUser>): void {
+		const lastItem = this.fnQueue.length > 0 ? this.fnQueue[this.fnQueue.length - 1] : undefined;
+		if (lastItem && lastItem.fn === fn) return;
+		this.fnQueue.push({ fn, data });
 	}
 }
