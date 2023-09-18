378907d9b3a3e9cfb80315fd81c4250797e9f00c
Pietro Lungarini <pietro@lungarini.it>
2023-09-18 16:38:44 +0200

    feat(home): add Product Hunt banner component

--- src/app/core/pages/home/home.module.ts
+++ src/app/core/pages/home/home.module.ts
@@ -5,6 +5,7 @@ import { ImgixAngularModule } from '@imgix/angular';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { IdeasComponent } from './components/ideas/ideas.component';
 import { LatestChatsComponent } from './components/latest-chats/latest-chats.component';
+import { PhBannerComponent } from './components/ph-banner/ph-banner.component';
 import { ProfileOverviewComponent } from './components/profile-overview/profile-overview.component';
 import { SubscriptionOverviewComponent } from './components/subscription-overview/subscription-overview.component';
 import { HomeRoutingModule } from './home-routing.module';
@@ -18,6 +19,7 @@ import { HomeComponent } from './home.component';
 		LatestChatsComponent,
 		IdeasComponent,
   	SubscriptionOverviewComponent,
+   PhBannerComponent,
 	],
   imports: [
     CommonModule,
