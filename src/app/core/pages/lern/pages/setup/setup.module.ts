6a187a087d5570745ff5d4965c25cc8e193e18cd
Pietro Lungarini <pietro@lungarini.it>
2023-09-20 14:38:42 +0200

    feat(setup): add setup module with routing and component

---
+++ src/app/core/pages/lern/pages/setup/setup.module.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { SharedModule } from 'src/app/shared/shared.module';
+import { IntroComponent } from './pages/intro/intro.component';
+import { SetupRoutingModule } from './setup-routing.module';
+import { SetupComponent } from './setup.component';
+
+
+@NgModule({
+  declarations: [
+    SetupComponent,
+    IntroComponent,
+  ],
+  imports: [
+    CommonModule,
+		SetupRoutingModule,
+		SharedModule,
+  ]
+})
+export class SetupModule { }
