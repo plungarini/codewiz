1d10a2268343a5f06e2b2c7ba996c7f9da01dec5
Pietro Lungarini <pietro@lungarini.it>
2023-09-21 14:57:26 +0200

    feat: add replace-env-var script to prepare for ci-cd global variables

---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+export const environment = {
+  production: '%#PRODUCTION#%',
+  firebase: {
+    projectId: '%#FB_PROJECTID#%',
+    appId: '%#FB_APPID#%',
+    storageBucket: '%#FB_STORAGEBUCKET#%',
+    locationId: '%#FB_LOCATIONID#%',
+    apiKey: '%#FB_APIKEY#%',
+    authDomain: '%#FB_AUTHDOMAIN#%',
+    messagingSenderId: '%#FB_MESSAGINGSENDERID#%',
+    measurementId: '%#FB_MEASUREMENTID#%',
+  },
+  supabase: {
+    url: '%#SB_URL#%',
+    projectRef: '%#SB_PROJECTREF#%',
+    anonKey: '%#SB_ANONKEY#%',
+  }
+};
