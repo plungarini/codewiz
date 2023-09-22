932adafef8eaa0d92fa0c0e20476af9a594f2610
Pietro Lungarini <pietro@lungarini.it>
2023-09-22 18:10:06 +0200

    refactor: Remove unused imports and variables in setup component

--- src/app/core/pages/lern/pages/setup/setup.component.ts
+++ src/app/core/pages/lern/pages/setup/setup.component.ts
@@ -1,6 +1,4 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { LernService } from '../../services/lern.service';
 
 @Component({
   selector: 'app-setup',
@@ -15,19 +13,5 @@ import { LernService } from '../../services/lern.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SetupComponent {
-
-	private courseId: string;
-
-	constructor(
-		private lern: LernService,
-		private route: ActivatedRoute,
-	) {
-		this.courseId = this.route.snapshot.params['id'] || 'new';
-	}
-
-	async searchDocs(query: string) {
-		const res = await this.lern.searchDocs(query);
-		console.log(res);
-	}
 
 }
