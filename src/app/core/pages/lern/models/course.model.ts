36e202fac9e4ba3abec5ae7b5d6e089f65c42fa7
Pietro Lungarini <pietro@lungarini.it>
2023-09-22 18:00:45 +0200

    feat(course.model): Add LernStepTopic type and SearchDocsResponse type

--- src/app/core/pages/lern/models/course.model.ts
+++ src/app/core/pages/lern/models/course.model.ts
@@ -1,11 +1,30 @@
 import { Timestamp } from '@angular/fire/firestore';
 
+type LernStepTopic = {
+	query: string;
+	pages: SearchDocsResponse['pages'];
+	res: { can: boolean, suggested?: string }
+}
+
 export type LernCourse = {
 	id?: string;
 	repo: string;
 	owner: string;
 	name: string;
+	steps?: {
+		topic?: LernStepTopic;
+	};
 	status: 'private' | 'public';
 	createdAt: Timestamp;
 	updatedAt: Timestamp;
 }
+
+export type SearchDocsResponse = {
+	can: boolean;
+	pages: {
+		id: string;
+		title: string;
+		content: string;
+	}[];
+	suggested?: string;
+};
