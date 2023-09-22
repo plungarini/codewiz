f6c021c7409cf5f9e0627a0b7d32287ebee98aff
Pietro Lungarini <pietro@lungarini.it>
2023-09-22 18:10:21 +0200

    refactor: Remove unused PromptComponent from SetupModule

--- src/app/core/pages/lern/pages/setup/setup.module.ts
+++ src/app/core/pages/lern/pages/setup/setup.module.ts
@@ -5,19 +5,19 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { IntroComponent } from './pages/intro/intro.component';
 import { SetupRoutingModule } from './setup-routing.module';
 import { SetupComponent } from './setup.component';
-import { PromptComponent } from './pages/prompt/prompt.component';
 
 
 @NgModule({
   declarations: [
     SetupComponent,
     IntroComponent,
-    PromptComponent,
+    
   ],
   imports: [
     CommonModule,
 		SetupRoutingModule,
 		SharedModule,
+		
   ]
 })
 export class SetupModule { }
