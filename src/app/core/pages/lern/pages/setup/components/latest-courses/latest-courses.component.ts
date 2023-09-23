ed57be9442c6289793d67c000390f38a867449ee
Pietro Lungarini <pietro@lungarini.it>
2023-09-24 00:59:41 +0200

    feat: add LatestCoursesComponent

---
+++ src/app/core/pages/lern/pages/setup/components/latest-courses/latest-courses.component.ts
@@ -0,0 +1,92 @@
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UserRepoService } from 'src/app/core/pages/chat/services/user-repo.service';
+import { Repo } from 'src/app/shared/models/repo.model';
+import { LernCourse } from '../../../../models/course.model';
+import { LernService } from '../../../../services/lern.service';
+
+@Component({
+  selector: 'app-latest-courses',
+  templateUrl: './latest-courses.component.html',
+  styles: [
+    `
+      :host {
+        display: block;
+      }
+
+			@keyframes aicolors {
+				0% {
+					stroke: #818cf8;
+				}
+				33.33% {
+					stroke: #ec4899;
+				}
+				66.66% {
+					stroke: #0369a1;
+				}
+				100% {
+					stroke: #818cf8;
+				}
+			}
+			.animate-aicolors {
+				animation: aicolors 5s linear infinite;
+			}
+    `
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LatestCoursesComponent implements OnDestroy {
+
+	loaded = false;
+
+	courses: LernCourse[] = [];
+	repos$ = this.repos.getAllSupportedDocs();
+
+	private coursesSub: Subscription;
+
+	constructor(
+		private cdRef: ChangeDetectorRef,
+		private lern: LernService,
+		private repos: UserRepoService,
+	) {
+		this.coursesSub = this.lern.getAll(10)
+			.subscribe((courses) => {
+				if (!this.loaded) this.loaded = true;
+
+				this.courses = courses;
+				this.cdRef.markForCheck();
+			})
+	}
+
+	ngOnDestroy(): void {
+		this.coursesSub.unsubscribe();
+	}
+
+	getRepoLogo(id: string, repos: Repo[] | null): string {
+		if (!repos) return '';
+		return repos.find(r => r.id === id)?.logo ?? '';
+	}
+
+	getTimeDifference(date?: Date): string {
+		if (!date) return 'Few secs ago';
+		const currentTime = new Date();
+		const diffInMinutes = Math.floor((currentTime.getTime() - date.getTime()) / (1000 * 60));
+		const diffInHours = Math.floor(diffInMinutes / 60);
+		const diffInDays = Math.floor(diffInHours / 24);
+
+		if (diffInMinutes < 1) {
+			return 'just now';
+		} else if (diffInMinutes < 60) {
+			return `${diffInMinutes} min${diffInMinutes > 1 ? 's' : ''} ago`;
+		} else if (diffInHours < 24) {
+			return `${diffInHours} hour${diffInHours > 1 ? 's' : ''} ago`;
+		} else {
+			return `${diffInDays} day${diffInDays > 1 ? 's' : ''} ago`;
+		}
+	}
+
+	trackBy(i: number, item: LernCourse): string {
+		return item.id || i.toString();
+	}
+
+}
--- src/app/core/pages/lern/pages/setup/setup.module.ts
+++ src/app/core/pages/lern/pages/setup/setup.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { ImgixAngularModule } from '@imgix/angular';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { LatestCoursesComponent } from './components/latest-courses/latest-courses.component';
+import { FinishedComponent } from './pages/finished/finished.component';
 import { IntroComponent } from './pages/intro/intro.component';
 import { SetupRoutingModule } from './setup-routing.module';
 import { SetupComponent } from './setup.component';
-import { FinishedComponent } from './pages/finished/finished.component';
 
 
 @NgModule({
@@ -13,13 +15,14 @@ import { FinishedComponent } from './pages/finished/finished.component';
     SetupComponent,
     IntroComponent,
     FinishedComponent,
+    LatestCoursesComponent,
     
   ],
   imports: [
     CommonModule,
 		SetupRoutingModule,
 		SharedModule,
-		
+		ImgixAngularModule,
   ]
 })
 export class SetupModule { }
