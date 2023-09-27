50419a5e6a08c68f9f7a6cc05ee45d6fc4fb6eab
Pietro Lungarini <pietro@lungarini.it>
2023-09-27 14:56:30 +0200

    refactor: Improve code readability in utils/index.ts

--- functions/src/utils/index.ts
+++ functions/src/utils/index.ts
@@ -3,10 +3,14 @@ import { warn } from 'firebase-functions/logger';
 
 admin.initializeApp();
 
+
 export const firestore = admin.firestore();
+firestore.settings({ ignoreUndefinedProperties: true });
+
 export const auth = admin.auth();
+
 export const production = () => {
-	const prod = process.env.GCLOUD_PROJECT?.includes('-staging') ? false : true;
+	const prod = !process.env.GCLOUD_PROJECT?.includes('-staging');
 	if (!prod) warn('[DEV ENVIRONMENT] Prevented to execute function.');
 	return prod;
 };
