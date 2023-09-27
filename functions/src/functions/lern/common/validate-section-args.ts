a0b20b971f1d568558837cfb004a4164e9ef496a
Pietro Lungarini <pietro@lungarini.it>
2023-09-27 14:57:27 +0200

    feat(validate-section-args): add function to validate course section arguments

---
+++ functions/src/functions/lern/common/validate-section-args.ts
@@ -0,0 +1,121 @@
+import { warn } from 'firebase-functions/logger';
+import * as json5 from 'json5';
+import { LernCourseGenerationSection } from '../models/lern.model';
+
+export const validateCourseSectionArgs = (
+  args?: string,
+): LernCourseGenerationSection | undefined => {
+	if (!args) {
+		warn('No args received', 'HALLUCINATING');
+		return undefined;
+	}
+
+  warn('Validating args', args);
+
+  const normArgs = typeof args === 'string' ? parseArgs(args) : args;
+	if (!normArgs) {
+		warn('Unable to parse JSON', 'HALLUCINATING');
+		return undefined;
+	}
+
+	if (!areQuizOptionsValid(normArgs.quiz)) {
+		warn('Invalid quiz options', 'HALLUCINATING');
+		return undefined;
+	}
+
+	if (!isAssignmentValid(normArgs.assignment)) {
+		warn('Invalid assignment', 'HALLUCINATING');
+		return undefined;
+	}
+
+	if (!isValidSectionName(normArgs.sectionTitle)) {
+		warn('Invalid section name', 'HALLUCINATING');
+		return undefined;
+	}
+
+	if (!isContentValid(normArgs.content)) {
+		warn('Invalid content', 'HALLUCINATING');
+		return undefined;
+	}
+
+	if (!isValidTldr(normArgs.tldr)) {
+		warn('Invalid TL;DR', 'HALLUCINATING');
+		return undefined;
+	}
+
+	return {
+		sectionTitle: normArgs.sectionTitle,
+		content: normArgs.content,
+		quiz: normQuiz(normArgs.quiz),
+		assignment: normArgs.assignment,
+		tldr: normArgs.tldr,
+  };
+};
+
+const parseArgs = (args: string): LernCourseGenerationSection | undefined => {
+	// Regular expression to remove trailing commas
+	const regex = /,\s*([\]}])/gm;
+
+	// Regular expression to remove double commas (,,)
+	const doubleCommaRegex = /,\s*(,)/gm;
+
+	// Remove trailing commas and try to parse
+	const sanitizedData = args.replace(regex, '$1').replace(doubleCommaRegex, '$1');
+
+  try {
+    return json5.parse(sanitizedData);
+  } catch (err) {
+    return undefined;
+  }
+};
+
+const isValidSectionName = (sectionName: string | undefined): boolean => {
+  return !!sectionName && typeof sectionName === 'string';
+};
+
+const isContentValid = (content: string): boolean => {
+	return !!content && typeof content === 'string';
+};
+
+const areQuizOptionsValid = (quiz: LernCourseGenerationSection['quiz']): boolean => {
+  if (!quiz) return true;
+
+	if (!['single', 'multi'].includes(quiz.quizType)) {
+		return false;
+	}
+
+	const quizzes = normQuiz(quiz);
+	const optionsValid = (quizzes?.options.length ?? 0) > 0 && quizzes?.options.every((option) => {
+		return !!option.option &&
+			typeof option.option === 'string' &&
+			option.isCorrect !== undefined &&
+			(!option.whyNotCorrect || typeof option.whyNotCorrect === 'string');
+	});
+
+  return !!optionsValid;
+};
+
+const isAssignmentValid = (assignment: LernCourseGenerationSection['assignment']): boolean => {
+	if (!assignment) return true;
+	return typeof assignment === 'string';
+};
+
+const isValidTldr = (tldr: string): boolean => {
+	return !!tldr && typeof tldr === 'string';
+};
+
+const normQuiz = (quiz: LernCourseGenerationSection['quiz']): LernCourseGenerationSection['quiz'] => {
+	return quiz ? {
+		question: quiz.question,
+		quizType: quiz.quizType,
+		options: quiz.options
+			.filter((option) => !!option.option && typeof option.option === 'string' && typeof option.isCorrect === 'boolean')
+			.map((option) => {
+				return {
+					option: option.option,
+					isCorrect: !!option.isCorrect,
+					whyNotCorrect: option.whyNotCorrect ?? '',
+				};
+			}),
+	} : undefined;
+};
