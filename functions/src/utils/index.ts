37048377688f6a4c6de52a6589b22f59dd5b4154
Pietro Lungarini <pietro@lungarini.it>
2023-10-02 16:30:28 +0200

    refactor: Remove unnecessary code and simplify production function

--- functions/src/utils/index.ts
+++ functions/src/utils/index.ts
@@ -1,5 +1,4 @@
 import * as admin from 'firebase-admin';
-import { warn } from 'firebase-functions/logger';
 
 admin.initializeApp();
 
@@ -10,7 +9,5 @@ firestore.settings({ ignoreUndefinedProperties: true });
 export const auth = admin.auth();
 
 export const production = () => {
-	const prod = !process.env.GCLOUD_PROJECT?.includes('-staging');
-	if (!prod) warn('[DEV ENVIRONMENT] Prevented to execute function.');
-	return prod;
+	return !process.env.GCLOUD_PROJECT?.includes('-staging');
 };
