fbeca31a8c2a24d9c36189d408dcee370f2756d6
Pietro Lungarini <pietro@lungarini.it>
2023-10-02 19:00:50 +0200

    refactor: Move latest-courses component to the shared module

--- src/app/shared/components/latest-courses/latest-courses.component.ts
+++ src/app/shared/components/latest-courses/latest-courses.component.ts
@@ -2,8 +2,8 @@ import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy } from
 import { Subscription } from 'rxjs';
 import { UserRepoService } from 'src/app/core/pages/chat/services/user-repo.service';
 import { Repo } from 'src/app/shared/models/repo.model';
-import { LernCourseRequest } from '../../../../models/course.model';
-import { LernService } from '../../../../services/lern.service';
+import { LernCourseRequest } from '../../../core/pages/lern/models/course.model';
+import { LernService } from '../../../core/pages/lern/services/lern.service';
 
 @Component({
   selector: 'app-latest-courses',
