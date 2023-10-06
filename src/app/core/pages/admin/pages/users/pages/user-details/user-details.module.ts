6e54ff4f6aabec3ab7d9d33dc16207af65fbe126
Pietro Lungarini <pietro@lungarini.it>
2023-10-06 16:17:13 +0200

    feat(user-details): add user roles component and import ReactiveFormsModule

--- src/app/core/pages/admin/pages/users/pages/user-details/user-details.module.ts
+++ src/app/core/pages/admin/pages/users/pages/user-details/user-details.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { ImgixAngularModule } from '@imgix/angular';
 import { UserActionsComponent } from './components/user-actions/user-actions.component';
 import { UserHeaderComponent } from './components/user-header/user-header.component';
 import { UserOverviewComponent } from './components/user-overview/user-overview.component';
+import { UserRolesComponent } from './components/user-roles/user-roles.component';
 import { UserDetailsRoutingModule } from './user-details-routing.module';
 import { UserDetailsComponent } from './user-details.component';
 
@@ -15,11 +17,13 @@ import { UserDetailsComponent } from './user-details.component';
 		UserHeaderComponent,
 		UserOverviewComponent,
 		UserActionsComponent,
+  	UserRolesComponent,
   ],
   imports: [
     CommonModule,
 		UserDetailsRoutingModule,
 		ImgixAngularModule,
+		ReactiveFormsModule,
   ]
 })
 export class UserDetailsModule { }
