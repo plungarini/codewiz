828dbb9eb847dd6f02eb03634f38be30a3436c41
Pietro Lungarini <pietro@lungarini.it>
2023-10-07 13:58:53 +0200

    feat: add personalized meta tags for lern pages

--- src/app/core/pages/lern/pages/lern-unauthorized/lern-unauthorized.component.ts
+++ src/app/core/pages/lern/pages/lern-unauthorized/lern-unauthorized.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { PersonalMetaTagsService } from 'src/app/shared/services/personal-meta-tags.service';
 
 @Component({
   selector: 'app-lern-unauthorized',
@@ -13,5 +14,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LernUnauthorizedComponent {
+
+	constructor(
+		private meta: PersonalMetaTagsService,
+	) {
+		this.meta.update({ title: 'CodeWiz | WizLern - Not Authorized' });
+	}
 
 }
