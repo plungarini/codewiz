efca4aaee9860f0438f7c282f5d9199ba6521dc2
Pietro Lungarini <pietro@lungarini.it>
2023-10-07 13:08:03 +0200

    feat: Add feedbacks functionality to lern-feedbacks component

--- src/app/core/pages/admin/pages/feedbacks/components/lern-feedbacks/lern-feedbacks.component.ts
+++ src/app/core/pages/admin/pages/feedbacks/components/lern-feedbacks/lern-feedbacks.component.ts
@@ -1,4 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { LernFeedback } from '../../models/lern-feedback.model';
+import { LernFeedbacksService } from '../../services/lern-feedbacks.service';
 
 @Component({
   selector: 'app-lern-feedbacks',
@@ -13,5 +16,17 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LernFeedbacksComponent {
+
+	feedbacks$: Observable<LernFeedback[]> = this.lernFeedbacks.getAll();
+
+	showFeedback$ = new BehaviorSubject<LernFeedback | undefined>(undefined);
+
+	constructor(
+		private lernFeedbacks: LernFeedbacksService,
+	) { }
+
+	showFeedback(feedback: LernFeedback | undefined) {
+		this.showFeedback$.next(feedback);
+	}
 
 }
