1a32858667375855da79c52d326490ecef7fbc07
Pietro Lungarini <pietro@lungarini.it>
2023-10-07 11:32:03 +0200

    feat: Update app-feedbacks component to display feedbacks and show individual feedbacks

--- src/app/core/pages/admin/pages/feedbacks/components/app-feedbacks/app-feedbacks.component.ts
+++ src/app/core/pages/admin/pages/feedbacks/components/app-feedbacks/app-feedbacks.component.ts
@@ -1,4 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { AppFeedback } from '../../models/app-feedback.model';
+import { AppFeedbacksService } from '../../services/app-feedbacks.service';
 
 @Component({
   selector: 'app-app-feedbacks',
@@ -13,5 +16,17 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppFeedbacksComponent {
+
+	feedbacks$ = this.feedbacks.getAll();
+
+	showFeedback$ = new BehaviorSubject<AppFeedback | undefined>(undefined);
+
+	constructor(
+		private feedbacks: AppFeedbacksService,
+	) { }
+
+	showFeedback(feedback: AppFeedback | undefined) {
+		this.showFeedback$.next(feedback);
+	}
 
 }
