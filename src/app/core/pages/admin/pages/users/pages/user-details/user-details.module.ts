b5eafce4c174c78eabdaa422372bb44f8be4746a
Pietro Lungarini <pietro@lungarini.it>
2023-10-07 10:49:32 +0200

    feat(user-details): Add user chats component and markdown module to user-details module

--- src/app/core/pages/admin/pages/users/pages/user-details/user-details.module.ts
+++ src/app/core/pages/admin/pages/users/pages/user-details/user-details.module.ts
@@ -3,7 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { ReactiveFormsModule } from '@angular/forms';
 import { ImgixAngularModule } from '@imgix/angular';
+import { MarkdownModule } from 'ngx-markdown';
 import { UserActionsComponent } from './components/user-actions/user-actions.component';
+import { UserChatsComponent } from './components/user-chats/user-chats.component';
 import { UserHeaderComponent } from './components/user-header/user-header.component';
 import { UserOverviewComponent } from './components/user-overview/user-overview.component';
 import { UserRolesComponent } from './components/user-roles/user-roles.component';
@@ -18,12 +20,14 @@ import { UserDetailsComponent } from './user-details.component';
 		UserOverviewComponent,
 		UserActionsComponent,
   	UserRolesComponent,
+   	UserChatsComponent,
   ],
   imports: [
     CommonModule,
 		UserDetailsRoutingModule,
 		ImgixAngularModule,
 		ReactiveFormsModule,
+		MarkdownModule,
   ]
 })
 export class UserDetailsModule { }
